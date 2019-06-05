c4fc05cd65ecd36bf27ddffcffb450a969b80045
Lizz <eamyers217@gmail.com>
2019-06-05 11:19:33 -0500

    help

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -41,17 +41,17 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-            if (parts[3] == "standard")
+            if (parts[2] == "standard")
             {
                  gradeBook = new StandardGradeBook(name);
             }
-            else if (parts[3] == "ranked")
+            else if (parts[2] == "ranked")
             {
                  gradeBook = new RankedGradeBook(name);
             }
             else
             {
-                Console.WriteLine(parts[3] + " is not a supported type of gradebook, please try again");
+                Console.WriteLine(parts[2] + " is not a supported type of gradebook, please try again");
                 return;
             }
            
@@ -81,7 +81,7 @@ namespace GradeBook.UserInterfaces
             Console.WriteLine();
             Console.WriteLine("GradeBook accepts the following commands:");
             Console.WriteLine();
-            Console.WriteLine("Create 'Name' - Creates a new gradebook where 'Name' is the name of the gradebook.");
+            Console.WriteLine("Create 'Name' 'Type' - Creates a new gradebook where 'Name' is the name of the gradebook and 'Type' is what type of grading it should use.");
             Console.WriteLine();
             Console.WriteLine("Load 'Name' - Loads the gradebook with the provided 'Name'.");
             Console.WriteLine();
